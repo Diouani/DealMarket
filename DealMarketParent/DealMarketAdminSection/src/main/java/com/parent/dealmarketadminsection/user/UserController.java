3a74ad4a20651ed9eee1c210f435fef43da1d13e
Diouani <diouani.youssef@gmail.com>
2022-06-09 22:44:50 +0100

    Adding some methods to userService and UserController

--- DealMarketParent/DealMarketAdminSection/src/main/java/com/parent/dealmarketadminsection/user/UserController.java
+++ DealMarketParent/DealMarketAdminSection/src/main/java/com/parent/dealmarketadminsection/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
 
 import java.util.*;
 
@@ -17,18 +18,39 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("")
-    public String ListAll(Model model){
+    public String listAll(Model model) {
 
-       List<User> userList = userService.listAll();
-       model.addAttribute("listUsers",userList);
+        List<User> userList = userService.listAll();
+        model.addAttribute("listUsers", userList);
         return "users";
     }
 
     @GetMapping("/new")
-    public String newUser(){
+    public String newUser(Model model) {
+
+
+        List<Role> listRoles = userService.listRoles();
+
+
+        User user = new User();
+        user.setEnabled(true);
+
+
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+        model.addAttribute("pageTitle", "Create New User");
+
         return "user_form";
     }
 
+
+    @PostMapping("/save")
+    public String saveUser(User user , RedirectAttributes redirectAttributes) {
+        System.out.println(user);
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message" , "The User has been saved successfully.");
+        return "redirect:/users";
+    }
 
 
 }
--- DealMarketParent/DealMarketAdminSection/src/main/java/com/parent/dealmarketadminsection/user/UserService.java
+++ DealMarketParent/DealMarketAdminSection/src/main/java/com/parent/dealmarketadminsection/user/UserService.java
@@ -13,10 +13,23 @@ public class UserService  {
     @Autowired
     private UserRepository  userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
 
     public List<User> listAll(){
         return (List<User>) userRepository.findAll();
     }
 
 
+
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
