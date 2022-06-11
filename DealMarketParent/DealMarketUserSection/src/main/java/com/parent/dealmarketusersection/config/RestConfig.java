1b03e14d0a4f7c7cc568d14241f4879e2d8bff46
Diouani <73253036+Diouani@users.noreply.github.com>
2022-06-11 21:27:14 +0200

    Create RestConfig.java
    adding restconfig to ignore Some http methods

---
+++ DealMarketParent/DealMarketUserSection/src/main/java/com/parent/dealmarketusersection/config/RestConfig.java
@@ -0,0 +1,36 @@
+package com.parent.dealmarketusersection.config;
+
+import com.root.dealmarketshared.entity.Product;
+import com.root.dealmarketshared.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+@Configuration
+public class RestConfig  implements RepositoryRestConfigurer {
+
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+
+        // gestion des exposition de la classe produit
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+        // gestion des exposition de la classe produitcategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+
+    }
+}
